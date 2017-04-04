d15713d1894d408ce669bcf0b22dbd6cbb52fd21
SaiRamTej <ram66.puppala@gmail.com>
2017-04-04 23:44:17 +0530

    created new mvc application and added to github

---
+++ TestGitMVCApp/TestGitMVCApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestGitMVCApp.Startup))]
+namespace TestGitMVCApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
